222fc9c6ded7223bdaccfbdff0debd7e885fe6a6
bugbit <ohernandezba@gmail.com>
2017-05-13 09:16:10 +0200

    Generador 1-100

---
+++ AlgoritmosDotNet/AlgoritmosDotNet.CifrasYLetras.Shared/Generador/GeneraCifras1A100.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AlgoritmosDotNet.Comun;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoritmosDotNet.CifrasYLetras.Generador
+{
+	public class GeneraCifras1A100: IGeneradorCifras
+	{
+		private RandomRNG mRandom = new RandomRNG ();
+
+		#region IGeneradorCifras implementation
+
+		public Enunciado Generar ()
+		{
+			var pNumeros = new List<int> ();
+
+			return new Enunciado {
+				Numeros = Enumerable.Range (1, 6).Aggregate (pNumeros, (l, n) => {
+					l.Add (1 + mRandom.Next (100));
+					return l;
+				}).ToArray (),
+				Objetivo = 100 + mRandom.Next (900)
+			};
+		}
+
+		#endregion
+	}
+}
+
